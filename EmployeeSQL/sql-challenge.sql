2ad87f3a3aa08049ce9c8691804f942cf7f2bc06
JMazick <jazlynmm@outlook.com>
2020-12-10 08:01:59 -0600

    update file

--- EmployeeSQL/sql-challenge.sql
+++ EmployeeSQL/sql-challenge.sql
@@ -6,7 +6,8 @@ SELECT *
 FROM "salaries"
 
 -- perform inner join with employees and salaries table
+CREATE TABLE "table_one" AS
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM "salaries"
 INNER JOIN "employees" ON
-employees.emp_no=salaries.emp_no;
+employees.emp_no = salaries.emp_no;
