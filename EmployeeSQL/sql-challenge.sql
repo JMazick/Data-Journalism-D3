0d9f2f76394354a7a36c03ae35c13e7de9fe4fdf
JMazick <jazlynmm@outlook.com>
2020-12-10 17:19:23 -0600

    update

--- EmployeeSQL/sql-challenge.sql
+++ EmployeeSQL/sql-challenge.sql
@@ -18,3 +18,22 @@ FROM "table_one"
 -- List first name, last name, and hire date for employees who were hired in 1986
 SELECT YEAR(hire_date)
 FROM "employees"
+
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name
+SELECT * FROM "dept_manager"
+SELECT * FROM "departments"
+
+CREATE TABLE "table_3_1" AS
+SELECT dept_manager.dept_no, employees.emp_no, employees.last_name, employees.first_name 
+FROM "dept_manager"
+INNER JOIN "employees" ON employees.emp_no = dept_manager.emp_no;
+
+
+SELECT * FROM "table_3_1"
+
+CREATE TABLE "table_3" AS
+SELECT table_3_1.dept_no, departments.dept_name, table_3_1.emp_no, table_3_1.last_name, table_3_1.first_name
+FROM "departments"
+INNER JOIN "table_3_1" ON table_3_1.dept_no = departments.dept_no;
+
+SELECT * FROM "table_3"
